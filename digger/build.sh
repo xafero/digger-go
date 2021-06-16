aadffc459c65b99350937b910cee77b4251a7a25
xafero <gberendt@outlook.com>
2021-06-16 23:35:07 +0200

    Adding resources to build script

--- digger/build.sh
+++ digger/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # REQ: sudo apt-get install libgtk-3-dev
+# REQ: https://github.com/kevinburke/go-bindata
 
 go get github.com/gotk3/gotk3/gtk
 go get github.com/gotk3/gotk3/gdk
@@ -8,6 +9,8 @@ go get github.com/gotk3/gotk3/glib
 go get github.com/gotk3/gotk3/cairo
 go get golang.design/x/thread
 go get log
+
+go-bindata -pkg diggerclassic -o ./diggerclassic/assets.go -prefix res res/...
 
 go build launcher.go
 mv launcher goDigger
